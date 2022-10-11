cbe26a4615c02c4f56a986752b171fa3b6e40b07
Avezz78 <113528051+Avezz78@users.noreply.github.com>
2022-10-11 23:08:24 +0530

    Add files via upload
    task

---
+++ Bank.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDetails
+{
+    internal class Bank : IAccount
+    {
+        public void Accountdetails()
+        {
+            Console.WriteLine("setting Account details");
+        }
+
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; } // access modifier protected
+
+        public Bank()
+        {
+        }
+
+        public Bank(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank b = new Bank(1234,"sahil",5000.0);
+            Console.WriteLine("Account number:"+b.Number);
+            Console.WriteLine("Account holder name:"+b.Holder);
+            Console.WriteLine("Account Balance:"+b.Balance);
+
+            b.Deposit(5000.0);
+            Console.WriteLine("Account balance after depositing:"+b.Balance);
+
+            b.Withdraw(1000.0);
+            Console.WriteLine("Account balance after withdrawing:"+b.Balance);
+        }
+    }
+}
